403e2bf0731ba9398ab1e6b3e0b40607d729cc17
Hugo Pinheiro <hugo_hp10@hotmail.com>
2021-01-20 09:11:41 -0300

    Repetição While

---
+++ While.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+// While, Verifica uma condição ate ela ser verdadeira.
+
+var i = 10
+while i < 10
+{
+    print(i)
+    i += 1
+}
+
+print("-----------")
+
+
+// Repeat, Realiza a instrução e depois chega a condição
+
+i = 10
+repeat
+{
+    print(i)
+    i += 1
+} while (i < 10)
+
+// Do, while em outras linguagens 
