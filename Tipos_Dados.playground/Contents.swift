174c14e96a098f61e47cb2c1187995afe86d69ec
Hugo Pinheiro <hugo_hp10@hotmail.com>
2021-01-19 21:48:45 -0300

    Tipos de dados

---
+++ Tipos_Dados.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+// TIPOS DE DADOS
+
+let int = 1     // int -> Inteiro
+let double = 3.14   //Double -> Ponto flutuante
+let string = "Hugo"     // String-> Textos
+let isDrive = true     // Booleano -> Condição(true ou false)
+
+
+// Atribuindo o tipo escolhido a uma varíavel
+let number: Int = 2
+let pi: Double = 3.14
+let name: String = "Hugo"
+let no: Bool = true
