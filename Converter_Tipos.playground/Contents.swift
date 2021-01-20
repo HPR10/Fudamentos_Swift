16bc55cd97bd539de9f2877c8646338507169f5d
Hugo Pinheiro <hugo_hp10@hotmail.com>
2021-01-19 21:51:15 -0300

    Convertendo dados

---
+++ Converter_Tipos.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+// CONVERTER TIPOS DE DADOS
+
+let x = 10
+let name = String(x)
+print(name)
+
+// CASTING -> Pratica de converter um dado em outro tipo.
+
+let y = "abc"
+let value = Int(y)
+print(value)        // Retorna um valor (nil / null)
