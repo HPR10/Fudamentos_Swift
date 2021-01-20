7caa0273c228effb5e257bf3d7ce1997f715518f
Hugo Pinheiro <hugo_hp10@hotmail.com>
2021-01-19 21:46:30 -0300

    Tipos básicos de variávies

---
+++ Tipos_Variaveis.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// TIPOS DE VARIÁVEIS
+
+// var = variável mutável para armazenar dados
+var message = "Olá, mundo"
+message = "Dev, Hugo Pinheiro"
+
+// let = (constante) É uma variável imutável para armazenar dados
+let name = "Hugo"
+print(name)  // Impressão na saída padrão
+
+
+
+// TIPOS DE DADOS
+
+LET INT = 1
+LET DOUBLE
