0f82f686a62e4b698e7aedd012dd24bd6e3c0ab8
Hugo Pinheiro <hugo_hp10@hotmail.com>
2021-01-20 19:00:36 -0300

    Primeiro exercicio em Swift

---
+++ Exercicio_01.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var cidade = "Angra Dos Reis"
+let anoNascimento = 1991
+
+print("Eu nasci na cidade de  ",cidade + " " + "no ano de", anoNascimento )
+print("Eu nasci na cidade \(cidade) no ano de \(anoNascimento)")
+
