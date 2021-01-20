3a502b3d5fcfc61d3d9539e3f321f5cc074ac120
Hugo Pinheiro <hugo_hp10@hotmail.com>
2021-01-19 21:52:55 -0300

    Operadores e incrementos

---
+++ Operadores_Incrementos.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+// INCREMENTOS, OPERADORES LOGICOS
+
+let result = 2 <= 3
+
+let x = 10
+let y = 20
+let res = x == y
+
+let firstName = "Hugo"
+let lastName = "Pinheiro"
+firstName >= lastName
+
+// Igual(==), Diferente(!=), Atribuição(=), Menor(<), Maior(>), Maior_Igual(>=), Menor_Igual(<=)
+
+// Inversão(!), and(&&), or(||)
+
+let isStudant = true
+let isFederal = true
+
+!isStudant
+isStudant && 5 > 3
+let r = isFederal || isStudant
+print(r)
+
+// Incremento ( + && -)
+var z = 10
+z += 2
+var w = 8
+w -= 5
+
+
