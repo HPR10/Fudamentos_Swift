7b5d3964210f7c553dbcbcf95bd0ecd3200520cb
Hugo Pinheiro <hugo_hp10@hotmail.com>
2021-01-20 18:59:31 -0300

    Dicas no console

---
+++ Console.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+let primeiroNome = "Hugo"
+let ultimoNome = "Raimundo"
+  
+// Método print()
+  
+// Concatenação
+  
+print(primeiroNome + " " + ultimoNome)
+  
+// Interpolação
+print("Meu primeiro nome é \(primeiroNome) e o meu ultimo nome é \(ultimoNome)")
+
+var numero1 = 10
+var numero2 = 20
+print("O primeiro número é: \(numero1). E o segundo número é \(numero2). E a soma dos dois números é: \(numero1 + numero2)")
