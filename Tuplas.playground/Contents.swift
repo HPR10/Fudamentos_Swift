1898761d144146378b8f6f26e9d5da444b382da2
Hugo Pinheiro <hugo_hp10@hotmail.com>
2021-01-23 19:57:21 -0300

    Tuplas básico

--- Tuplas.playground/Contents.swift
+++ Tuplas.playground/Contents.swift
@@ -1,7 +1,10 @@
-import UIKit
+//  Tuplas ou Tuples
+
+// Uma tupla pode ser de diversos data types diferentes. Depois de declaradas não é possível adicionar ou remover valores de uma tupla.
 
 let latitude:Double = 23.21
 
+// Atribuindo valores de forma explicita
 let coords: (Double, Double) = (23.4, 54.22)
 
 // Acessando os valores por indice
@@ -9,7 +12,7 @@ coords.0
 coords.1
 
 
-// Acessando os valores pelo nome atribuido
+// Acessando os valores pelo nome atribuido de forma implicita
 let coords2 = (lat: 23,2, lng: 54.22)
 coords2.lat
 coords2.lng
@@ -33,3 +36,13 @@ print(b)
 let user = (name: "Hugo", age: 29)
 user.age
 user.name
+
+// Tupla explicita com função e indetificadores
+
+var pessoa: (nome:String,idade:Int,corOlhos:String) = ("Pan",2 ,"pretos")
+
+func nomeIdade(tupla: (nome:String,idade:Int,corOlhos:String))
+{
+    print("\(tupla.nome) tem \(tupla.idade) anos de idade e olhos \(tupla.corOlhos)")
+}
+nomeIdade(tupla: pessoa)
