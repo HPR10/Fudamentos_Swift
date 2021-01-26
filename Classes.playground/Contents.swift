f58ddb25b7cc2bff91e4dcc19757b4c4caa10a7e
Hugo Pinheiro <hugo_hp10@hotmail.com>
2021-01-25 22:22:52 -0300

    Classes em Swift

--- Classes.playground/Contents.swift
+++ Classes.playground/Contents.swift
@@ -1,5 +1,33 @@
 // Classes
 
+//Inicializadores customizados
+
+class Carro
+{
+    var marca = String()        // Forma elegante de representar String Vazia.
+    var modelo = String()
+    var velocidade = Int()
+    var tipo = String()
+
+    init (){      // Inicializador de objetos, caso não declarado como agora é executado no background
+    }
+    
+    init (marcaDoCarro: String, modeloDoCarro: String)
+    {
+        marca = marcaDoCarro
+        modelo =  modeloDoCarro
+    }
+}
+
+var tesla = Carro()
+tesla.marca
+tesla.modelo
+
+var tesla2 = Carro(marcaDoCarro: "Tesla", modeloDoCarro: "Model 3")
+tesla2.marca
+tesla2.modelo
+
+
 class Mesa          // Com uma única classe, podemos criar vários objetos, só a primeira letra de uma classe é maiuscula
 {
     var cor = ""
@@ -22,8 +50,18 @@ var areaDaMesa: Double
         let volume: Double = area * Double(alturaP)/100
         return volume
     }
-
+    
+    init(){
+    }
+    
+    init(alturaDamesa: Int, larguraDamesa: Int, comprimentoDaMesa: Int)
+    {
+        altura = alturaDamesa
+        largura = larguraDamesa
+        comprimento = comprimentoDaMesa
+    }
 }
+
 var mesaDaSala = Mesa()          // Criando o primeiro Objeto
 mesaDaSala.altura = 130
 mesaDaSala.comprimento = 500
@@ -31,6 +69,11 @@ mesaDaSala.cor = "Verde"
 mesaDaSala.largura = 300
 mesaDaSala.numerodePernas = 10
 mesaDaSala.areaDaMesa
+
+
+var mesaDaSala2 = Mesa(alturaDamesa: 150, larguraDamesa: 300, comprimentoDaMesa: 500)   //Versão elegante e otimizada
+
+
 
 mesaDaSala.calcularVolume(area: mesaDaSala.areaDaMesa, alturaP: mesaDaSala.altura)
 
@@ -54,11 +97,11 @@ mesaDaCozinha.areaDaMesa
 
 
 
-/*var mesasDaCasa: [Mesa] = [mesaDoQuarto, mesaDaSala, mesaDaCozinha]
+var mesasDaCasa: [Mesa] = [mesaDoQuarto, mesaDaSala, mesaDaCozinha]
 print("Qual é a cor de cada mesa?")
 for mesa in mesasDaCasa
 {
     print(mesa.cor)
 }
-*/
+
 
