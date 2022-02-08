c1de03e6ce48524cce81955bf45239fc23855663
Hugo Pinheiro <hugo_hp10@hotmail.com>
2022-02-08 20:08:14 -0300

    Add func generic and data structs

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -1,4 +1,5 @@
 import Cocoa
+import Foundation
 
 // Matrizes, Dicionários, Opcioanais e Result são genéricos em swift.
 
@@ -60,7 +61,45 @@ let result1 = cast("Avada Kedrava")
 let result2 = cast("flowers")
 
 
-// https://www.raywenderlich.com/3535703-swift-generics-tutorial-getting-started
+// MARK: - ESTRUTURA DE DADOS
+
+// Queue é um tipo genérico com um argumento tipo, em sua cláusula de argumento genérico. Outra maneira de dizer isso é genérico sobre tipo.
+
+struct Queue<Element> {
+    
+    private var elements: [Element] = []
+    
+    mutating func enqueue(newElement: Element) {
+        elements.append(newElement)
+    }
+    
+    mutating func dequeue() -> Element? {
+        guard !elements.isEmpty else {return nil}
+        return elements.remove(at: 0)
+    }
+}
+
+var q = Queue<Int>()
+
+q.enqueue(newElement: 4)
+q.enqueue(newElement: 2)
+q.enqueue(newElement: 5)
+q.enqueue(newElement: 1)
+
+q.dequeue()
+q.dequeue()
+q.dequeue()
+q.dequeue()
+
+// MARK: - FUNÇÕES GENÉRICAS
+
+func pairs<key, value>(from dictionary: [key: value]) -> [(key, value)] {
+    return Array(dictionary)
+}
+
+let somePairs = pairs(from: ["minimum": 199, "maximum": 299])
+let somePairsTwo = pairs(from: [1: "Swift", 2: "Java", 3: "Kotlin"])
+
 
 
 
